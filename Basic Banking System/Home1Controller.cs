b620333fd09307e0c40d1333bf44fda7bf8cc300
PriyaPc5 <94124486+PriyaPc5@users.noreply.github.com>
2021-11-20 19:52:54 +0530

    Add files via upload
    # BasicBankingSystem
    Sparks Foundation Web Development Internship Project : Basic Banking System website. 
    A web application used for basic banking transactions
    
    ## The website has the following specification and  -
    Scripting languages:HTML, CSS, Javascript
    Backend : SQL
    Framework :MVC
    Other Language :ASP.NET
    IDE's :Visual Studio 2019,SQL Server Management studio(SSMS)

---
+++ Basic Banking System/Home1Controller.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BankingSystem.Models;                                                                                                                                                                                                                          
+
+namespace BankingSystem.Controllers
+{
+    public class Home1Controller : Controller
+    {
+        db_accountsEntities db = new db_accountsEntities();
+        // GET: Home1
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(tbl_accounts obj, string btn)
+        {
+            if (btn== "Withdraw")
+            {
+                var data = db.tbl_accounts.Where(obj1 => obj1.acc_no == obj.acc_no).FirstOrDefault();
+
+                if (obj.amt <= obj.amt)
+                {
+                    data.amt -= obj.amt;
+                    int mess = db.SaveChanges();
+
+                    if (mess == 1)
+                    {
+                        ViewBag.data = "Withdraw Done";
+                    }
+                    else
+                    {
+                        ViewBag.data = "Withdraw Not Done";
+                    }
+                }
+                else
+                {
+                    ViewBag.data = "Insufficient Balance";
+                }
+            }
+            else if (btn== "Deposite")
+            {
+                var data = db.tbl_accounts.Where(obj1 => obj1.acc_no == obj.acc_no).FirstOrDefault();
+                data.amt += obj.amt;
+               int mess = db.SaveChanges();
+                if (mess == 1)
+                {
+                    ViewBag.data = "Deposite Done";
+                }
+                else
+                {
+                    ViewBag.data = "Deposite Not Done";
+                }
+            }
+            else if (btn== "Check Balance")
+            {
+                var data = db.tbl_accounts.Where(obj1 => obj1.acc_no == obj.acc_no).FirstOrDefault();
+                ViewBag.show = data.amt;
+            }
+            return View();
+        }
+    }
+}
